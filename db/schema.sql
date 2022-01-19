41a6491bb9876cb0a36f440e24e3117c46ad288f
kai <kai@kaiconsulting.co>
2022-01-19 16:01:43 -0700

    inserted data to seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,33 @@ CREATE DATABASE cms_db;
 USE cms_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    -- salary DECIMAL ,
-        -- The syntax is DECIMAL(M,D)
-        -- M - total length
-        -- D - digits right of the decimal point
-    department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    salary DECIMAL(6),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,29 @@ INSERT INTO department (id, name)
 VALUES (1, "Sales"),
     (2, "Engineering"),
     (3, "Legal"),
-    (4, "Finance");
+    (4, "Finance"),
+    (5, "Marketing");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 100000, 1),
+    (2, "Salesperson", 80000, 1),
+    (3, "Lead Engineer", 150000, 2),
+    (4, "Software Engineer", 120000, 2),
+    (5, "Account Manager", 160000, 4),
+    (6, "Accountant", 125000, 4),
+    (7, "Legal Team Lead", 250000, 3),
+    (8, "Lawyer", 190000, 3),
+    (9, "Marketing Lead", 90000, 5),
+    (10, "Marketing Intern", 40000, 5);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "John", "Doe", 1, NULL),
+    (2, "Mike", "Chan", 2, 1),
+    (3, "Ashley", "Rodriguez", 3, NULL),
+    (4, "Kevin", "Tupik", 4, 3),
+    (5, "Kunal", "Singh", 5, NULL),
+    (6, "Malia", "Brown", 6, 5),
+    (7, "Sarah", "Lourd", 7, NULL),
+    (8, "Tom", "Allen", 8, 7),
+    (9, "Kai", "Walsh", 9, NULL),
+    (10, "Lara", "Vanderbilt", 10, 9);
