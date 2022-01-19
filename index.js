98aba3c49bbf59e524d5ba08f00d3a9885849945
kai <kai@kaiconsulting.co>
2022-01-19 16:25:45 -0700

    added viewTable and started writing functions

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const db = mysql.createConnection({
     database: 'cms_db',
 }).promise();
 
-const readFromFile = util.promisify(db.query);
+const viewTable = ( table ) => {
+	console.log( '\n' );
+	console.table( table );
+	console.log( '\n' );
+};
 
 // ALL QUESTIONS - ask what action the user wants to take
 const allQuestions = () => {
@@ -24,32 +28,55 @@ const allQuestions = () => {
             },
         ])
         .then(answers => {
-            if(answers.home === "View all departments"){
-                viewAllDepartments();
-            } else if(answers.home === "View all roles"){
+            console.log(answers);
+            // if(answers.home === "View all departments"){
+            //     viewAllDepartments();
+            // } else if(answers.home === "View all roles"){
 
-            } else if(answers.home === "View all employees"){
+            // } else if(answers.home === "View all employees"){
 
-            } else if(answers.home === "Add a department"){
+            // } else if(answers.home === "Add a department"){
 
-            } else if(answers.home === "Add a role"){
+            // } else if(answers.home === "Add a role"){
 
-            } else if(answers.home === "Add an employee"){
+            // } else if(answers.home === "Add an employee"){
 
-            } else if (answers.home === "Update an employee role"){
+            // } else if (answers.home === "Update an employee role"){
 
-            }
+            // }
         });
 };
 
-const viewAllDepartments = () => {
-
+const viewAllDepartments = async () => {
+    try {
+        const table = await db.query(queries.department);
+        viewTable(table);
+        return 
+    } catch (e) {
+        console.log(e);
+    }
 };
 
-const viewAllRoles = () => {
+// const viewAllRoles = () => {
 
-};
+// };
 
-const viewAllEmployees = () => {
+// const viewAllEmployees = () => {
 
-};
+// };
+
+// const addDepartment = () => {
+
+// };
+
+// const addRole = () => {
+
+// };
+
+// const addEmployee = () => {
+
+// };
+
+// const updateEmployeeRole = () => {
+
+// };
